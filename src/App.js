c303d2e274e127f1c817f61f9babe430ebf4384b
techsithgit <hemilp@tuplecorp.com>
2019-07-07 11:12:44 +0100

    initial

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+import Item from "./components/item";
+
+const initList = [
+  { name: "tomato", calorie: 20 },
+  { name: "rice", calorie: 30 },
+  { name: "candy", calorie: 100 }
+];
 
 function App() {
+  const [list, setList] = useState(initList);
+  const [editable, setEditable] = useState(false);
+
+  function removeItemHandle(e) {
+    const filteredList = list.filter(v => v.name !== e.target.name);
+    setList(filteredList);
+  }
+  function makeEditableHandle() {
+    setEditable(true);
+  }
+  function keyPressHandle(e, i) {
+    if (e.key === "Enter") {
+      setEditable(!editable);
+      const copyList = [...list];
+      copyList[i].name = e.target.value;
+    }
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h2>Grocery List</h2>
+        {list.map((v, k) => {
+          return (
+            <Item
+              key={`${k}${v.name}${v.calorie}`}
+              item={v}
+              onClick={removeItemHandle}
+              editable={editable}
+              onDoubleClick={makeEditableHandle}
+              onKeyPress={keyPressHandle}
+              index={k}
+            />
+          );
+        })}
       </header>
     </div>
   );
